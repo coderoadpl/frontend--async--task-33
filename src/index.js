cebb4b3e225a71f24dc1869a1ff718299fe4e518
Mateusz Choma <kontakt@coderoad.pl>
2021-02-15 16:39:28 +0100

    Login button added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+import { Button } from './components'
+
 import { signIn } from './auth'
 
-console.log('Hello CodeRoad!')
-
-signIn('example@example.com', 'fake-password')
+const button1 = new Button('Login (wrong credentials)', () => {
+    signIn('example@example.com', 'fake-password')
     .then((data) => console.log('wrong credentials resolved', data))
     .catch((error) => console.error('wrong credentials rejected', error))
+})
 
-signIn('kontakt@coderoad.pl', 'secret')
-.then((data) => console.log('good credentials resolved', data))
-.catch((error) => console.error('good credentials rejected', error))
+const button2 = new Button('Login (good credentials)', () => {
+    signIn('kontakt@coderoad.pl', 'secret')
+        .then((data) => console.log('good credentials resolved', data))
+        .catch((error) => console.error('good credentials rejected', error))
+})
+
+document.body.appendChild(button1.render())
+document.body.appendChild(button2.render())
